1332747fad527ab58b976cddc2092ec66668cc0f
Nikolay <Onedaydejavu124@mail.ru>
2022-08-27 18:55:39 +0700

    Дано число. Проверить кратно ли оно 7. Решение при помощи функции

---
+++ EX003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23. Решение при помощи функции
+
+bool Multiple(int x)
+{
+    int y = 7;
+    bool result = true;
+    if (x % y == 0)
+        result = true;
+    else
+        result = false;
+return result;
+}
+
+Console.WriteLine("Ввидите число: ");
+int x = int.Parse(Console.ReadLine());
+
+if(Multiple(x))
+    Console.WriteLine($"Число {x} кратно 7");
+else
+    Console.WriteLine($"Число {x} не кратно 7");
